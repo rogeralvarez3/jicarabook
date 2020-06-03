34508641d234e51d4097eb429379ae6deb400769
roger alvarez <rogeralvarez3@gmail.com>
2020-06-03 09:44:26 -0600

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,45 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+    <v-navigation-drawer clipped app width="200">
+      <v-img>
+        <img :src="$store.state.user.image" alt="Foto de perfil" class="pa-2 ma-2">
+      </v-img>
+      <v-list-item dense v-for="mn in menu" :key="mn.value">
+        <v-list-item-icon>
+          <v-icon v-text="mn.icon"></v-icon>
+        </v-list-item-icon>
+        <v-list-item-title v-text="mn.text"></v-list-item-title>
+      </v-list-item>
+    </v-navigation-drawer>
+    <v-app-bar app clipped-left dense color="primary" dark class="elevation-2">
+      <v-app-bar-nav-icon class="hidden-lg-and-up"></v-app-bar-nav-icon>
+      Jícara<strong>BOOK</strong>
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+     
     </v-app-bar>
 
     <v-content>
-      <HelloWorld />
+      
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+
 
 export default {
   name: "App",
-
-  components: {
-    HelloWorld
-  },
-
   data: () => ({
-    //
+    menu:[
+      {value:1,text:"Perfil",icon:"mdi-account"},
+      {value:2,text:"Noticias",icon:"mdi-newspaper"},
+      {value:3,text:"Fotos",icon:"mdi-camera-burst"},
+      {value:4,text:"Vídeos",icon:"mdi-video"},
+      {value:5,text:"Notas",icon:"mdi-file"},
+      {value:6,text:"Opciones",icon:"mdi-cog"},
+      {value:7,text:"Salir",icon:"mdi-close-circle"},
+    ]
   })
 };
 </script>
